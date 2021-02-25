5e979530b76a587e549d096056dc5bb3cd93f4d1
xi-97 <76947587+xi-97@users.noreply.github.com>
2021-02-25 12:48:57 +0800

    Add files via upload

---
+++ 优先队列2.cpp
@@ -0,0 +1,33 @@
+//ÓÅÏÈ¶ÓÁÐ
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+struct fruit
+{
+	string name;
+	int price;
+	
+}f1,f2,f3;
+struct cmp
+{
+	bool operator()(fruit f1,fruit f2)
+	{
+		return f1.price > f2.price;
+	}
+};
+int main()
+{
+	priority_queue<fruit,vector<fruit>,cmp>q;
+	f1.name="桃子";
+	f1.price=3;
+	f2.name="梨子";
+	f2.price =4;
+	f3.name="苹果";
+	f3.price=1;
+	q.push(f1);
+	q.push(f2);
+	q.push(f3);
+	cout<<q.top().name<<" "<<q.top().price<<endl;
+	return 0;
+}
